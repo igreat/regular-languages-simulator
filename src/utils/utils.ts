1f31537c72924e0906aefebe986da6cf841654d4
igreat <goraguy5@gmail.com>
2024-09-11 15:24:55 +0400

    fix annoying lint issues

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -3,10 +3,10 @@ import type { DFAJson } from "../simulator/dfa";
 import * as d3 from "d3";
 
 export function DFAJsonToDFAData(data: DFAJson): DFAData {
-    let nodes: d3.SimulationNodeDatum[] = Array.from({ length: Object.keys(data.table).length }, () => ({}));
-    let nodeLabels: string[] = Object.keys(data.table);
-    let links: d3.SimulationLinkDatum<d3.SimulationNodeDatum>[] = [];
-    let linkLabels: string[] = [];
+    const nodes: d3.SimulationNodeDatum[] = Array.from({ length: Object.keys(data.table).length }, () => ({}));
+    const  nodeLabels: string[] = Object.keys(data.table);
+    const links: d3.SimulationLinkDatum<d3.SimulationNodeDatum>[] = [];
+    const linkLabels: string[] = [];
     for (const [source, targets] of Object.entries(data.table)) {
         for (const [symbol, target] of Object.entries(targets)) {
             links.push({ source: Number(source), target });
