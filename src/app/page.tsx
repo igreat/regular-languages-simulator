fd12bd2cc1b29cab32fcd69fad101be203780f25
igreat <goraguy5@gmail.com>
2024-09-23 22:26:23 +0100

    add force dynamic + vscode stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import '~/styles/globals.css';
 import MainPage from './MainPage';
 import { db } from "~/server/db";
 
+export const dynamic = "force-dynamic";
+
 export default async function HomePage() {
   return (
     <MainPage />
